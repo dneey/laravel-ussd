9b8eb13871f756eca2fb54da44bcddc0b8e449f7
David Yartey <yarteyd@gmail.com>
2020-05-18 23:21:03 +0000

    return response with header

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -9,6 +9,9 @@ class UssdController extends Controller
     public function callback()
     {
         Log::info('Ussd application');
-        return 'done';
+       return response()->json(['success' => 1])
+       ->withHeaders([
+           'Access-Control-Allow-Origin' => 'http://apps.Hubtel.com',
+       ]);
     }
 }
